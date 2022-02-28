3aff7b58c72d0208993abb06c4fd478166b0fb72
alistarsabe <alistairtarirosabe@gmail.com>
2022-02-28 07:19:44 +0200

    Begin

--- app/src/main/java/com/alistair/businesscard/MainActivity.kt
+++ app/src/main/java/com/alistair/businesscard/MainActivity.kt
@@ -3,15 +3,18 @@ package com.alistair.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alistair.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +36,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
-    Surface( modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+            .height(390.dp)
+            .padding(14.dp)
+            , shape = RoundedCornerShape(corner = CornerSize(15.dp))
+            , elevation = 4.dp) {
 
+        }
 
     }
 
