c5c59737afe3584a179fb8e48c80db15948a90a4
alistarsabe <alistairtarirosabe@gmail.com>
2022-04-03 09:12:31 +0200

    Add Column & Divider & Button

--- app/src/main/java/com/alistair/businesscard/MainActivity.kt
+++ app/src/main/java/com/alistair/businesscard/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.alistair.businesscard
 
+import android.graphics.Paint
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alistair.businesscard.ui.theme.BusinessCardTheme
@@ -39,11 +45,34 @@ fun CreateBizCard(){
     Surface( modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
+        Card(modifier = Modifier
+            .width(200.dp)
             .height(390.dp)
             .padding(14.dp)
             , shape = RoundedCornerShape(corner = CornerSize(15.dp))
             , elevation = 4.dp) {
+
+            Column(modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                CreateProfileImage()
+                Divider()
+                CreateInfo()
+
+                    Button(
+                        onClick = {
+                            Log.d("Clicked", "CreateBizCard: Clicked")
+                        }){
+                        Text(text = "Portfolio",
+                        style = MaterialTheme.typography.button)
+
+
+
+                    }
+
+
+            }
 
         }
 
@@ -51,6 +80,50 @@ fun CreateBizCard(){
 
 }
 
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Miles P.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        Text(
+            text = "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
+
+        Text(
+            text = "@alistairCompose",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+
+    }
+}
+
+@Composable
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
